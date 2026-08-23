61964abb87b245733c1a9b6046554bff86c94c27
CocoGermany <cocogermany.ytd@gmail.com>
2026-08-23 18:33:15 +0530

    Update project

--- js/db.js
+++ js/db.js
@@ -21,7 +21,7 @@ import {
   runTransaction 
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
-const FidoDB = {
+export const FidoDB = {
   // --- 1. Projects ---
   async getProjects(filter = {}) {
     const projectsRef = collection(db, "projects");
